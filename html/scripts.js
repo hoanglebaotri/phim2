e38574a35c045f767efbbd9f317730b1be110c25
hoanglebaotri <91924806+hoanglebaotri@users.noreply.github.com>
2023-12-09 16:45:04 +0700

    th

---
+++ html/scripts.js
@@ -0,0 +1,8 @@
+
+if (!localStorage.getItem('alertShown')) {
+    alert('Chào mừng bạn đến trang web!');
+    
+    // Đánh dấu rằng đã hiển thị thông báo
+    localStorage.setItem('alertShown', 'true');
+  }
+//   alert("con cặc")
